fbab2f60986f865fd3048f0f6257eb714ae8b52f
aradosla <anna.valeri.radoslavova@cern.ch>
2023-07-13 17:15:06 +0200

    First commit

---
+++ installme.sh
@@ -0,0 +1,12 @@
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+bash Miniforge3-Linux-x86_64.sh -b  -p ./miniforge -f
+source miniforge/bin/activate
+python -m pip install -r requirements.txt
+mkdir modules
+cd modules
+git clone https://github.com/xsuite/tree_maker.git
+python -m pip install -e tree_maker
+git clone https://github.com/xsuite/xmask.git
+pip install -e xmask
+cd ..
+xsuite-prebuild
